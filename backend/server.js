e77ce4910cffbd1016653e3d1b37a94b1730fca1
louis@programmer <62738677+louis103@users.noreply.github.com>
2024-07-08 12:40:57 +0300

    Configured signup and login functionality

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../code/pages", "index.html"));
 });
 
+// Get dashboard
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "../code/pages", "dashboard.html"));
+});
+
 // Serve the signup HTML file
 app.get("/signup", (req, res) => {
   res.sendFile(path.join(__dirname, "../code/pages", "signup.html"));
@@ -29,7 +34,12 @@ app.post("/signup", (req, res) => {
   const { username, email, full_name, password } = req.body;
   const query = `INSERT INTO user_details (user_name, email_id, full_name, password) VALUES ('${username}', '${email}', '${full_name}', '${password}')`;
   connection.query(query, (err, results) => {
-    if (err) throw err;
+    if (err) {
+      console.error("Error inserting user details:", err);
+      return res
+        .status(500)
+        .send({ error: "An error occurred while registering the user." });
+    }
     res.redirect("/");
   });
 });
@@ -38,11 +48,16 @@ app.post("/login", (req, res) => {
   const { email, password } = req.body;
   const query = `SELECT * FROM user_details WHERE email_id = '${email}' AND password = '${password}'`;
   connection.query(query, (err, results) => {
-    if (err) throw err;
+    if (err) {
+      console.error("Error verifying user details:", err);
+      return res
+        .status(500)
+        .send({ error: "An error occurred while login the user." });
+    }
     if (results.length > 0) {
       res.redirect("/home");
     } else {
-      res.send("Invalid login credentials");
+      res.send({ error: "Invalid login credentials" });
     }
   });
 });
